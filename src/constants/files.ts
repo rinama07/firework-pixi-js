34576312a299bf4f905ed9162ffd360b52e825ff
Marina <marinapq.code@gmail.com>
2022-12-03 19:59:05 +0000

    fix texture files path

--- src/constants/files.ts
+++ src/constants/files.ts
@@ -1,4 +1,4 @@
 export const FIREWORKS_XML_PATH = "fireworks.xml";
-export const FOUNTAIN_TEXTURE_PATH = "public/fountain.png";
-export const PARTICLE_TEXTURE_PATH = "public/particle.png";
-export const ROCKET_TEXTURE_PATH = "public/rocket.png";
+export const FOUNTAIN_TEXTURE_PATH = "/fountain.png";
+export const PARTICLE_TEXTURE_PATH = "/particle.png";
+export const ROCKET_TEXTURE_PATH = "/rocket.png";
